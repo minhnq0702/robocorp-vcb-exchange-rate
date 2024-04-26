d98d077f9f20437b109df02ed4e13f39430e036d
MingNe <minhnq.0702@gmail.com>
2024-04-26 14:54:23 +0700

    add robot task

---
+++ task/consumer.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+import os
+from collections.abc import Callable
+
+from robocorp import log, workitems
+from robocorp.tasks import task
+from RPA.Excel.Files import Files
+from RPA.Tables import Tables
+
+OUTPUT_FOLDER = "./output"
+OUT_VCB_RATE_FILE = "rate_data.xlsx"
+table = Tables()
+wb = Files()
+
+
+@task
+def minimal_task():
+    """
+    A minimal task that does nothing.
+    """
+    process_rate_data()
+
+
+def process_rate_data():
+    """Get rate data from work items
+
+    Returns:
+        tuple[str, list]: tuple of rate date and rate data
+    """
+    rate_date = None
+    rate_data = []
+    for item in workitems.inputs:
+        payload = item.payload
+
+        if isinstance(payload, dict):
+            try:
+                get_processor()(payload)
+            except Exception as e:
+                item.fail(str(e))
+            else:
+                item.done()
+
+    submit_data()
+
+    return rate_date, rate_data
+
+
+def get_processor() -> Callable[[dict[str, float | str]], None]:
+    """Return a processor function based on the environment
+
+    Returns:
+        Callable[[dict[str, float | str]], None]: Function to process rate data
+    """
+    return push_data_to_excel
+    # return push_data_to_api
+
+
+def push_data_to_excel(data: dict):
+    try:
+        wb.get_active_worksheet()
+    except:
+        wb.create_workbook(
+            os.path.join(OUTPUT_FOLDER, OUT_VCB_RATE_FILE),
+            sheet_name='rate_data',
+        )
+        wb.create_worksheet(
+            'rate_data',
+            exist_ok=True,
+            content=dict.fromkeys([
+                'rate_date', 'currency_code', 'buy', 'transfer', 'sell'
+            ]),
+            header=True,
+        )
+        wb.set_active_worksheet('rate_data')
+        wb.delete_rows(2)
+
+    wb.append_rows_to_worksheet(
+        data, 
+        header=True, 
+        formatting_as_empty=True,
+    )
+
+
+def push_data_to_api(data: dict):
+    pass
+
+
+def submit_data():
+    wb.save_workbook()
---
+++ task/producer.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+import datetime
+import xml.etree.cElementTree as ET
+from os import path
+from xml.etree.ElementTree import Element
+
+from robocorp import log, workitems
+from robocorp.tasks import task
+from RPA.HTTP import HTTP
+
+OUTPUT_FOLDER = "./output"
+VCB_URL = "https://portal.vietcombank.com.vn/Usercontrols/TVPortal.TyGia/pXML.aspx?b=10"
+VCB_RATE_FILE = "vcb_rate.xml"
+
+
+@task
+def minimal_task():
+    """
+    A minimal task that does nothing.
+    """
+    http = HTTP()
+    file_path = path.join(OUTPUT_FOLDER, VCB_RATE_FILE)
+    file = http.download(VCB_URL, target_file=file_path, overwrite=True)
+    log.info(file)
+    rate_date, rate_data = read_xml_data(file_path)
+    if rate_date and rate_data:
+        create_work_items(rate_date, rate_data)
+        log.info("Work items created successfully")
+
+
+def read_xml_data(file_path):
+    """Read data from XML file
+
+    Args:
+        file_path (str): XML file path
+
+    Returns:
+        tuple[str, list]: tuple of rate date and rate data
+    """
+    tree = ET.parse(file_path)
+    root = tree.getroot()
+
+    rate_date = None
+    rate_data = []
+    for child in root:
+        tag = child.tag
+        if tag == 'DateTime':
+            date_time = _get_datetime_element(child)
+            if not date_time:
+                continue
+            rate_date = date_time.strftime("%Y-%m-%d %H:%M:%S")
+            # workitems.outputs.create({
+            #     'date_time': date_time.strftime("%Y-%m-%d %H:%M:%S"),
+            # })
+        elif tag == 'Exrate':
+            code, buy, transfer, sell = _get_ex_rate(child)
+            if not code:
+                continue
+            rate_data.append({
+                'currency_code': code,
+                'buy': buy,
+                'transfer': transfer,
+                'sell': sell,
+            })
+    
+    if not rate_date:
+        log.info("No rate date found")
+        return None, None
+    return rate_date, rate_data
+
+
+def _get_datetime_element(xml_child: Element) -> datetime.datetime | None:
+    """Get datetime from XML element
+
+    Args:
+        xml_child (Element): Element contains datetime
+
+    Returns:
+        datetime.datetime | None: datetime object
+    """
+    format_str = "%m/%d/%Y %I:%M:%S %p"
+    dt_str = xml_child.text
+    if not dt_str:
+        return None
+
+    dt = datetime.datetime.strptime(dt_str, format_str)
+    return dt
+
+
+def _get_ex_rate(xml_child: Element) -> tuple[str, float | None, float | None, float | None]:
+    """
+    Get exchange rate from XML element
+
+    Args:
+        xml_child (Element): Element contains exchange rate
+
+    Returns:
+        tuple[str, float | None, float | None, float | None]: Tuple of Code, buy, transfer, sell exchange rate
+    """
+    ex_rate_str = xml_child.attrib
+
+    if not ex_rate_str:
+        return ('', None, None, None)
+
+    currency_code = ex_rate_str.get('CurrencyCode', None)
+    if currency_code is None:
+        return ('', None, None, None)
+
+    ex_buy = ex_rate_str.get('Buy', '')
+    ex_transfer = ex_rate_str.get('Transfer', '')
+    ex_sell = ex_rate_str.get('Sell', '')
+    return (
+        currency_code,
+        _check_currency_number_format(ex_buy),
+        _check_currency_number_format(ex_transfer),
+        _check_currency_number_format(ex_sell),
+    )
+
+
+def _check_currency_number_format(str_amount: str) -> float | None:
+    """
+    Check if the currency number is in correct format
+    Value is in Vietnam Currency Number Format: 25,118.00
+
+    Args:
+        str_amount (str): Currency number in string format
+
+    Returns:
+        float | None: Currency number in float format or None if the input is invalid
+    """
+    if str_amount:
+        try:
+            return float(str_amount.replace(',', ''))
+        except ValueError:
+            return None
+    return None
+
+
+def create_work_items(rate_date: str, rate_data: list[dict[str, float]]):
+    """Create work items
+
+    Args:
+        rate_date (str): Rate date
+        rate_data (list): List of rate data
+    """
+    for data in rate_data:
+        workitems.outputs.create({
+            'rate_date': rate_date,
+            **data,
+        })
+
+if __name__ == "__main__":
+    file_path = path.join(OUTPUT_FOLDER, VCB_RATE_FILE)
+    read_xml_data(file_path)
